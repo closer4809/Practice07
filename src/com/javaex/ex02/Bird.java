c593b8887d5dfd95ae8461c74c1ae9a87db1999b
benedict <closer9554@gmail.com>
2021-06-11 15:48:22 +0900

    6/10 과제

---
+++ src/com/javaex/ex02/Bird.java
@@ -0,0 +1,40 @@
+package com.javaex.ex02;
+
+public abstract class Bird {
+    protected String name;
+
+	public Bird() {
+		
+	}
+    
+    
+    public Bird(String name) {
+
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+	
+	public void sing() {
+	    
+    }
+
+    public void fly() {
+    
+    }
+    
+    public void showName() {
+    
+    }
+
+
+}
